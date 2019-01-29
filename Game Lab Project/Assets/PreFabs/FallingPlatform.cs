8f5b921a7a21ec91625898f7466ba4e660043183
RJAnthony <rjanthony98@gmail.com>
2019-01-29 18:41:57 -0500

    RJ added stuff
    added friction and fixed platforms

---
+++ Game Lab Project/Assets/PreFabs/FallingPlatform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidBody;
+    private BoxCollider2D boxcollider;
+
+    public float fallDelay;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        boxcollider = GetComponent<BoxCollider2D>();
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+            Destroy(gameObject, 3f);
+        }
+    }
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        myRigidBody.isKinematic = false;
+        boxcollider.isTrigger = true;
+
+
+        yield return 0;
+    }
+
+}
