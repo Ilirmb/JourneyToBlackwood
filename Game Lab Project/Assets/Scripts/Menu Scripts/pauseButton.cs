1c67668b03dad479ee93b5f1d058779b05860492
mgtsn <morgantosini@gmail.com>
2019-04-03 13:37:20 -0400

    Pause Functionality

---
+++ Game Lab Project/Assets/Scripts/Menu Scripts/pauseButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseButton : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    private bool paused = false;
+
+
+    public void Pause()
+    {
+        if (paused)
+        {
+            Time.timeScale = 1f;
+            paused = false;
+            pauseMenu.SetActive(false);
+        } else
+        {
+            Time.timeScale = 0f;
+            paused = true;
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
