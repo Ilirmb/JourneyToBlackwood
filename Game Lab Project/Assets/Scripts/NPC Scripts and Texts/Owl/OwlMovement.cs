0f0d88fb6e125cb977636549fc921f7336ebc1be
mgtsn <morgantosini@gmail.com>
2019-04-10 13:27:03 -0400

    Owl sprite + movement

--- Game Lab Project/Assets/Scripts/NPC Scripts and Texts/Owl/OwlMovement.cs
+++ Game Lab Project/Assets/Scripts/NPC Scripts and Texts/Owl/OwlMovement.cs
@@ -6,9 +6,9 @@ public class OwlMovement : MonoBehaviour {
 
     public Rigidbody2D player;
 
-    public float movespeed;
-
     public Rigidbody2D rb;
+
+    public SpriteRenderer sprite;
 
 	// Use this for initialization
 	void Start () {
@@ -20,9 +20,11 @@ public class OwlMovement : MonoBehaviour {
 		if (player.velocity.x > 0 && player.position.x > rb.position.x + 3)
         {
             rb.velocity = new Vector2(player.velocity.x, 0);
+            sprite.flipX = true;
         } else if (player.velocity.x < 0 && player.position.x < rb.position.x - 3)
         {
             rb.velocity = new Vector2(player.velocity.x, 0);
+            sprite.flipX = false;
         } else if (player.velocity.x == 0 && rb.velocity.x != 0)
         {
             new WaitForSeconds(2);
