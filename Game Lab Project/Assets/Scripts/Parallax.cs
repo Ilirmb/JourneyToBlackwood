943cdaba1e0591e9420efa0e8f76e341709bdae3
DemmyDev <DemmyPlays@gmail.com>
2019-04-16 19:48:08 -0400

    Adjusted parallaxing script to consider Y-axis
    Also deleted unneeded objects in Scene2

--- Game Lab Project/Assets/Scripts/Parallax.cs
+++ Game Lab Project/Assets/Scripts/Parallax.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour{
 
-    float length, startPos;
+    float length, startPosX, startPosY;
     Transform cam;
-    [SerializeField] float parallaxEffect;
+    [SerializeField] float parallaxEffectX, parallaxEffectY, limitsY;
 
     void Start()
     {
         cam = Camera.main.GetComponent<Transform>();
-        startPos = transform.position.x;
+        startPosX = transform.position.x;
+        startPosY = transform.localPosition.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     void Update()
     {
-        float temp = cam.position.x * (1 - parallaxEffect);
-        float dist = cam.position.x * parallaxEffect;
+        float temp = cam.position.x * (1 - parallaxEffectX);
+        float distX = cam.position.x * parallaxEffectX;
+        float distY = cam.position.y * parallaxEffectY;
 
-        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
-        if (temp > startPos + length) startPos += length;
-        else if (temp < startPos - length) startPos -= length;
+        transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z);
+
+        if (temp > startPosX + length) startPosX += length;
+        else if (temp < startPosX - length) startPosX -= length;
+
+        if (limitsY != 0)
+        {
+            if (transform.localPosition.y > limitsY) transform.localPosition = new Vector3(transform.localPosition.x, limitsY, transform.localPosition.z);
+            else if (transform.localPosition.y < -limitsY) transform.localPosition = new Vector3(transform.localPosition.x, -limitsY, transform.localPosition.z);
+        }
     }
 }
