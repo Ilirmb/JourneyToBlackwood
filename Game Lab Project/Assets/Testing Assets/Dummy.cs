1c96740ca1032296dfe65d648923c4e647e29dd3
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2019-05-29 08:44:14 -0400

    Update 5/28 9:00
    Tested switching sprite meshes at runtime. It's easier than I expected.

---
+++ Game Lab Project/Assets/Testing Assets/Dummy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Anima2D;
+
+public class Dummy : MonoBehaviour {
+
+    public SpriteMesh testMesh;
+    public SpriteMeshInstance switcheroo;
+
+	// Use this for initialization
+	void Start () {
+
+        switcheroo.spriteMesh = testMesh;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
