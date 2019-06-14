aa143775ce5ea22dd820e520f59de565104aa2b9
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2019-06-14 13:23:17 -0400

    Update? 6/14 - 1:15
    This isn't really getting anywhere, but I'm pushing anyway because why not.
    Botched tree structure. I'm going to do more research on how to make it not garbage.

---
+++ Game Lab Project/Assets/Dialogue/DialogueTree.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewTree", menuName = "Dialogue/Dialogue Tree", order = 2)]
+public class DialogueTree : ScriptableObject {
+
+    public DialogueNode parent;
+
+    private void OnEnable()
+    {
+
+    }
+
+}
+
+
+/// <summary>
+/// Represents a node in a dialogue tree. This could be either a series of options, or standard text 
+/// </summary>
+[System.Serializable]
+public class DialogueNode
+{
+    public enum NodeType { single, branch, error };
+
+    public NodeType dialogueNodeType = NodeType.error;
+    public string dialogueSpeaker;
+    public string dialogueText;
+    public Sprite dialogueSprite;
+
+    public List<int> childNodeIDs;
+
+    public void OnActivate() { }
+    public NodeType GetNodeType() { return dialogueNodeType; }
+}
