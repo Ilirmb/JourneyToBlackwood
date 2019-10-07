98e42ebb10f60c3b6e27ec5b028d760001031d39
RJAnthony <rjanthony98@gmail.com>
2019-10-07 18:04:42 -0400

    part2 of lvl to my branch
    part 2 of orchard lvl boxed out

---
+++ Orchard_Part2/Assets/Purchased Assets/3worlds/Scripts/UnderwaterUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnderwaterUI : MonoBehaviour {
+	private Dropdown waterSwitch;
+
+	[SerializeField]
+	private List<GameObject> options;
+
+	void Start () {
+		waterSwitch = GetComponent<Dropdown> ();
+		waterSwitch.captionText.text = "Water Type";
+
+	}
+	public void OnChange () {
+		var selectedIndex = waterSwitch.value;
+		foreach (var option in options) {
+			if (option != null) {
+				option.SetActive (false);
+			}
+		}
+		var gameObject = options[waterSwitch.value];
+		if (gameObject != null) {
+			gameObject.SetActive (true);
+		}
+	}
+
+}
