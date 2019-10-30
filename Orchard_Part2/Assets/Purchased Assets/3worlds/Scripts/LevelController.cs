63bc010d667b17602c0d28b44f5f74569b42e187
BrandonCoulombe <54905202+BrandonCoulombe@users.noreply.github.com>
2019-10-30 00:52:09 -0400

    Revert "Revert "Revert "Merge branch 'RJ_Orchardlvl'"""
    This reverts commit 2abbcb4064191092045b9a66f7c5749ff9176feb.

--- Orchard_Part2/Assets/Purchased Assets/3worlds/Scripts/LevelController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class LevelController : MonoBehaviour {
-
-	public void LoadScene (string sceneN) {
-		SceneManager.LoadScene (sceneN);
-	}
-}
