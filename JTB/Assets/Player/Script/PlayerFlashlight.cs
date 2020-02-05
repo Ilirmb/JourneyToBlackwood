fe996dd3684b7cd6426d270f49bb8c9468b2f2b9
phunk-tmurph <43594900+phunk-tmurph@users.noreply.github.com>
2020-02-05 03:04:36 -0500

    flashlight early
    working in editor but not play mode, crystals were bugged so i removed from the push just cause it needs work.

---
+++ JTB/Assets/Player/Script/PlayerFlashlight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlashlight : MonoBehaviour
+{
+    public GameObject torch;
+    private GameObject handler;
+    public bool canUse = true;
+    private Vector3 mousePos;
+
+    void Update()
+    { 
+        if (Input.GetMouseButton(0) && canUse == true)
+        {
+            canUse = false;
+            StartCoroutine(Flashlight());
+        }
+    }
+
+    IEnumerator Flashlight()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        handler = Instantiate(torch, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
+        handler.transform.position = mousePos;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Fadeout());
+    }
+
+    IEnumerator Fadeout()
+    {
+        yield return new WaitForSeconds(4);;
+        Reuse();
+        Destroy(handler.gameObject);
+    }
+
+    public void Reuse()
+    {
+        canUse = true;
+    }
+}
+
