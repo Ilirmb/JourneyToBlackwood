edcb998d112640898536a4555aabf03af6d20f5c
NoDienel <46758801+NoDienel@users.noreply.github.com>
2020-04-15 03:50:21 -0400

    fix to a lighting zone issue

--- JTB/Assets/LightZoneUpdater.cs
+++ JTB/Assets/LightZoneUpdater.cs
@@ -3,22 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// If the player dies and returns to a checkpoint, we want the zones to update to reflect that. This script essentially does that, by using the defined border to tell the thing to turn off. 
-/// 
-/// I'll probably refine this into a better system where each 'zone' knows it's start and end colliders and can command them to toggle nearby zones off or better yet, a general zone manager that can make sure the currently active zone is the only active zone. This is probably the best solution
+/// If the player dies and returns to a checkpoint, we want the zones to update to reflect that.
 /// </summary>
 
 public class LightZoneUpdater : MonoBehaviour
 {
-    [Header("Fill with the next Zone Colliders to make sure current light zone is active on respawn")]
-    public LightZoneManager PossiblyCrossedZoneBorder;
-    public bool isZoneB;
+    public LightGroup CurrentLightGroup;
 
     private void Awake()
     {
-        if(PossiblyCrossedZoneBorder == null)
+        if(CurrentLightGroup == null)
         {
-            //If there is no assigned LightZoneManager, there's no reason to bother with checking for collisions or doing comparisons
             this.enabled = false;
         }
     }
@@ -27,16 +22,10 @@ public class LightZoneUpdater : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if(PossiblyCrossedZoneBorder != null)
+            if(CurrentLightGroup != null)
             {
-                if (!isZoneB)
-                {
-                    PossiblyCrossedZoneBorder.ZoneAHit();
-                }
-                else
-                {
-                    PossiblyCrossedZoneBorder.ZoneBHit();
-                }
+                LightManager.Instance.ToggleAllOff();
+                CurrentLightGroup.ActivateLights();
             }
         }
     }
