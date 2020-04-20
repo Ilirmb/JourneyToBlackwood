ae205ff45ebecef3a2e908ca7c65633482cd2d68
phunk-tmurph <thomasmurphytsm@gmail.com>
2020-04-20 01:26:58 -0400

    forest parallax
    rgtygrdfx

--- JTB/Assets/Scripts/Parallax.cs
+++ JTB/Assets/Scripts/Parallax.cs
@@ -7,7 +7,7 @@ public class Parallax : MonoBehaviour{
 
     private float length, startPos;
     public GameObject cam;
-    public float pEffect, pEffectY; //how fast the bg moves
+    public float pEffect; //how fast the bg moves
 
     private void Start()
     {
@@ -21,8 +21,8 @@ public class Parallax : MonoBehaviour{
         float dist = (cam.transform.position.x * pEffect);
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
 
-        if (temp > startPos + length) startPos += length;
-        else if (temp < startPos - length) startPos -= length;
+        //if (temp > startPos + length) startPos += length;
+        //else if (temp < startPos - length) startPos -= length;
     }
 
 
