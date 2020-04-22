0d1d93942f923259fc9938547b4d78bb4afd1eca
phunk-tmurph <thomasmurphytsm@gmail.com>
2020-04-21 22:44:30 -0400

    door script
    yup very cool

---
+++ JTB/Assets/Scripts/DoorTeleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTeleport : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject exit;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        player.transform.position = new Vector2(exit.transform.position.x, exit.transform.position.y);
+        return null;
+    }
+}
