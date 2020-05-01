7bd07ca5599077f764928b487d3d17ee209319ab
phunk-tmurph <thomasmurphytsm@gmail.com>
2020-05-01 19:56:24 -0400

    extreme changes to lvl 1 / parallaxing
    pls dont move anything for the love of god. also "LVLONETESTDONT" scene is temporary but may need it so dont delete it

---
+++ JTB/Assets/Art Assets/Parallax Lvl1/parallaxtemptest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class parallaxtemptest : MonoBehaviour
+{
+
+    private float length, startPos;
+    public GameObject cam;
+    public float pEffect; //how fast the bg moves
+
+    private void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - pEffect));
+        float dist = (cam.transform.position.x * pEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        //if (temp > startPos + length && startPos < stopPos) startPos += length;
+        //else if (temp < startPos - length) startPos -= length;
+    }
+
+}
