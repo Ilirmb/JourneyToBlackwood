af93ef7125667ad47990b4d53901b6e0f2a0a530
Brian Coleman <signedcentaur@gmail.com>
2020-09-16 01:14:38 -0400

    Pause Menu Test

---
+++ JTB/Assets/Scripts/UI Scripts/PauseUIScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUIScript : MonoBehaviour
+{
+	//The State in where the game would be active
+    public static bool GameIsPaused = false;
+
+	public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (GameIsPaused)
+			{
+				Resume();
+			} 
+			else
+			{
+				Pause();
+			}
+		}
+    }
+
+	public void Resume()
+	{
+		PauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	public void Pause()
+	{
+		PauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	public void Restart()
+	{
+		Time.timeScale = 1f;
+		
+	}
+
+	public void Quitgame()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("Main Menu");
+	}
+}
