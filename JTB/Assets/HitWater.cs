e995870bdf4702fae01d800cf94fd5aaea918f2a
Stove414 <70985368+Stove414@users.noreply.github.com>
2020-09-15 22:07:51 -0400

    Updated Water Collision Script to reset position of Logs
    Added new function to the script that searches for all water logs and resets them to their default position and rotation

--- JTB/Assets/HitWater.cs
+++ JTB/Assets/HitWater.cs
@@ -7,16 +7,57 @@ public class HitWater : MonoBehaviour
     PlayerStatistics playerStatsScript;
     WaveManager waveManager;
     GameObject Player;
+
+    Vector3[] defaultPosition;
+    Quaternion[] defaultRotation;
+
+    Transform[] log;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Testing with Logs - Steve L 9/15/20
+        SetInitialPositions();
+
         Player = GameObject.FindWithTag("Player");
         playerStatsScript = GameObject.FindWithTag("Player").GetComponent<PlayerStatistics>();
         waveManager = GameObject.Find("Segment2").transform.GetChild(0).GetChild(0).GetComponent<WaveManager>();
     }
+
+
+    void SetInitialPositions()
+    {
+        //Search for all Logs
+        GameObject[] Logs = GameObject.FindGameObjectsWithTag("RiverLog");
+
+        //Create position and rotation with array size based on objects found
+        defaultPosition = new Vector3[Logs.Length];
+        defaultRotation = new Quaternion[Logs.Length];
+
+        log = new Transform[Logs.Length];
+
+        for (int i = 0; i < Logs.Length; i++)
+        {
+            log[i] = Logs[i].GetComponent<Transform>();
+
+            defaultPosition[i] = log[i].position;
+            defaultRotation[i] = log[i].rotation;
+        }
+    }
+
+    void resetLogs()
+    {
+        //Reset all logs back to each original position and rotation
+        for (int i = 0; i < log.Length; i++)
+        {
+            log[i].position = defaultPosition[i];
+            log[i].rotation = defaultRotation[i];
+        }
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             if (playerStatsScript.checkpoint == null)
             {
@@ -24,6 +65,7 @@ public class HitWater : MonoBehaviour
             }
             else
             {
+                resetLogs();
 
                 playerStatsScript.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 playerStatsScript.gameObject.transform.position = playerStatsScript.checkpoint.transform.position;
@@ -36,7 +78,7 @@ public class HitWater : MonoBehaviour
 
                 waveManager.StartCoroutine("DeleteWaves");
                 waveManager.rapidWaves = false;
-                
+
             }
 
             //if(playerStatsScript.gameObject != null)
