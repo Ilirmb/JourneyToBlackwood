d281a08b2defd86f904ae76bfaaed8c06d917627
Stove414 <70985368+Stove414@users.noreply.github.com>
2021-03-23 23:46:25 -0400

    Spikes, Rocks, killzones, hitboxes, and warp points

---
+++ JTB/Assets/RockDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDamage : MonoBehaviour
+{
+    GameObject Player;
+    PlayerStatistics playerStatsScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+        playerStatsScript = GameObject.FindWithTag("Player").GetComponent<PlayerStatistics>();
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerStatsScript.stamina -= 25;
+            playerStatsScript.CheckIfDead();
+
+            Object.Destroy(this.gameObject);
+        }
+    }
+}
