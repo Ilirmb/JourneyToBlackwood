a192c204f0b742d9c19b0e2798977f21c38ad23d
Stove414 <70985368+Stove414@users.noreply.github.com>
2021-04-01 16:45:01 -0400

    Hint Menus

---
+++ JTB/Assets/MenuHintShuffle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHintShuffle : MonoBehaviour
+{
+    Image hintmenu;
+    public Sprite[] MenuCards;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hintmenu = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hintmenu.sprite = current;
+    }
+
+    public void Shuffle()
+    {
+        hintmenu.sprite = MenuCards[Random.Range(0, MenuCards.Length)];
+    }
+}
