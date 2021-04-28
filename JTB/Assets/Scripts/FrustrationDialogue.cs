e88d2937d78ecda919c13532606185a89e0201b9
alecnercessian <55963562+alecnercessian@users.noreply.github.com>
2021-04-28 08:11:14 -0400

    started work on popup when frustrated script

---
+++ JTB/Assets/Scripts/FrustrationDialogue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrustrationDialogue : MonoBehaviour
+{
+    PlayerStatistics player;
+     string line;
+    DialogueManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+ // checks player frustration level
+        if (player.frustration >= 100f)
+        {
+            
+            Debug.Log("waiting to show dialogue");
+            new WaitForSeconds(30);
+            Debug.Log("Showing Dialogue");
+            manager.startDialogue();
+    }
+}
+    }
